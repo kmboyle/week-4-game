be53d4dfdbd290fedf048d69e7d7a31bc9ac5b0c
kmboyle <kevin.michael.boyle@gmail.com>
2017-06-18 10:07:05 -0400

    basic start

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+
+//create a character object
+var player = {
+	"name": " ",
+	"health": 0,
+	"attack": 0,
+	"counter": 0,
+	attackMove: function() {
+		attack *= attack;
+	}
+}
+//create a Darth player
+var player = {name: "Darth", health: "100", attack: 24, counter: 18};
+
+console.log(player.name);
