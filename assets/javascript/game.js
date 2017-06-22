f5a77f93e3f9fae03221206e96953151ffc393bb
kmboyle <kevin.michael.boyle@gmail.com>
2017-06-21 22:17:57 -0400

    all players fight

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,77 +4,147 @@
 $(document).ready(function() {
 
 //create a character object
-var player = {
-	"name": ["Darth Vader", "Han Solo", "Luke Skywalker", "Kylo Ren"],
-	"health": [140, 115, 130, 110],
-	"attack": [24,18,24,30],
-	"counter": [18,24,24,18],
-	attackPWUP: function(attack) {
-		return attack += attack;
-	},
-	"isEnemy": false,
-	"isPlayer": false,
-	"playerLock": false
-}
+// var player = {
+// 	"name": ["Darth Vader", "Han Solo", "Luke Skywalker", "Kylo Ren"],
+// 	"health": [140, 115, 130, 110],
+// 	"attack": [24,18,24,30],
+// 	"counter": [18,24,24,18],
+// 	attackPWUP: function(attack) {
+// 		return attack += attack;
+// 	},
+// 	"isEnemy": false,
+// 	"isPlayer": false,
+// 	"playerLock": false
+// }
 //create each player
-var playerD = {"name": "Darth", 
-				"health": 140, 
-				"attack": 6, 
-				"counter": 8, 
-				attackPWUP(){
-				return this.attack += this.attack;
+var playerD = {
+				name: "Darth", 
+				health: 140, 
+				attack: 6, 
+				counter: 14, 
+				attackPWUP: function(){
+				return this.attack += 6;
 	            },
-				"isEnemy": false, 
-				"isPlayer": false, 
-				"playerLock":false };
-var playerH = {"name": "Han Solo", 
-				"health": 115, 
-				"attack": 4, 
-				"counter": 10, 
-				attackPWUP(){
-				return this.attack += this.attack;
+				isEnemy: false, 
+				isPlayer: false, 
+				playerLock:false };
+var playerH = {
+				name: "Han Solo", 
+				health: 115, 
+				attack: 4, 
+				counter: 10, 
+				attackPWUP: function(){
+				return this.attack += 4;
 	            },
-				"isEnemy": false, 
-				"isPlayer": false, 
-				"playerLock":false };
-var playerL = {"name": "Luke Skywalker", 
-				"health": 130, 
-				"attack": 8, 
-				"counter": 6, 
-				attackPWUP(){
-				return this.attack += this.attack;
+				isEnemy: false, 
+				isPlayer: false, 
+				playerLock:false };
+var playerL = {
+				name: "Luke Skywalker", 
+				health: 130, 
+				attack: 8, 
+				counter: 6, 
+				attackPWUP: function(){
+				return this.attack += 8;
 	            },
-				"isEnemy": false, 
-				"isPlayer": false, 
-				"playerLock":false };
-var playerK = {"name": "Kylo Ren", 
-				"health": 110, 
-				"attack": 4, 
-				"counter": 10, 
-				attackPWUP(){
-				return this.attack += this.attack;
+				isEnemy: false, 
+				isPlayer: false, 
+				playerLock:false };
+var playerK = {
+				name: "Kylo Ren", 
+				health: 110, 
+				attack: 4, 
+				counter: 12, 
+				attackPWUP:function(){
+				return this.attack += 4;
 	            },
-				"isEnemy": false, 
-				"isPlayer": false, 
-				"playerLock":false };
+				isEnemy: false, 
+				isPlayer: false, 
+				playerLock:false };
 
+
+var play;//to hold players choice
+var enemy;//to hold enemy
+var count = 3; //hold enemy count
+
+//if player wins, have them pick another enemy.
+//  function enemy(){
+
+// $(".Player").on("click", function(){
+
+// if ($(this).hasClass("Darth"))
+// 		{
+// 			$(this).addClass("Defender");
+// 			playerD.playerLock = true;
+// 			$(".Defender").replaceWith(this);
+// 			$("h1").replaceWith($("<h1>Prepare For Battle</h1>"));
+// 			enemy = playerD;
+// 		}
+// 		else if ($(this).hasClass("Han"))
+// 		{
+// 			$(this).addClass("Defender");
+// 			playerH.playerLock = true;
+// 			$(".Defender").replaceWith(this);
+// 			$("h1").replaceWith($("<h1>Prepare For Battle</h1>"));
+// 			enemy = playerH;
+// 		}
+// 		else if ($(this).hasClass("Luke"))
+// 		{
+// 			$(this).addClass("Defender");
+// 			$(".pos3").replaceWith(this);
+// 			playerL.playerLock = true;
+// 			console.log(this);
+// 			$("h1").replaceWith($("<h1>Choose Your Enemy</h1>"));
+// 			enemy = playerL;
+// 		}
+// 		else if ($(this).hasClass("Kylo"))
+// 		{
+// 			$(this).addClass("Defender");
+// 			playerK.playerLock = true;
+// 			$(".Defender").replaceWith(this);
+// 			$("h1").replaceWith($("<h1>Choose Your Enemy</h1>"));
+// 			enemy = playerK;
+// 		}
+// 			console.log (enemy.name);
+// 	});
+	
+// }
 
 //$(".Health").html(player.name[0].health[0]);
 
 // console.log(player.attack[0]);
+function checkHealth(){
 
+		if (play.health <= 0) {
+		$("h1").replaceWith($("<h1 style=color:blue;><strong>You Lost!</strong></h1>"));
+	}
+
+	if (enemy.health <= 0) {
+		count--;
+		$("h1").replaceWith($("<h1 style=color:#26FF18;><strong>You Won! Select Another Enemy</strong></h1>"));
+		enemy.playerLock = false;
+		$(".Defender").replaceWith("<div class = pos2></div>");
+		$(".Defender").hide();
+		if (count === 0){
+		   	$("h1").replaceWith($("<h1 style=color:#26FF18;><strong>CONGRATULATIONS, YOU BEAT THE GAME....credits: KB</strong></h1>"));
+		   }
+		}
+	}
+
+//if player loses, have the game reset.
+
+
+
+//if player defeats all enemies, award game won and then reset.
 
 // console.log(player.attackPWUP(player.attack[0]));
 
-var play;//to hold players choice
-var enemy;//to hold enemy
-
-	
 
 	$(".DarthHP").html(playerD.health);
 	$(".HanHP").html(playerH.health);
 	$(".LukeHP").html(playerL.health);
 	$(".KyloHP").html(playerK.health);
+
 
 $(".Player").on("click", function() {
 
@@ -90,18 +160,18 @@ $(".Player").on("click", function() {
 		}
 		else if ($(this).hasClass("Han"))
 		{
-			$(this).addClass("Defender");
 			playerH.playerLock = true;
 			$(".pos2").replaceWith(this);
+			$(this).addClass("Defender");
 			$("h1").replaceWith($("<h1>Prepare For Battle</h1>"));
 			enemy = playerH;
 		}
 		else if ($(this).hasClass("Luke"))
 		{
-			$(this).addClass("Defender");
 			playerL.playerLock = true;
 			$(".pos2").replaceWith(this);
-			$("h1").replaceWith($("<h1>Choose Your Enemy</h1>"));
+			$(this).addClass("Defender");
+			$("h1").replaceWith($("<h1>Prepare For Battle</h1>"));
 			enemy = playerL;
 		}
 		else if ($(this).hasClass("Kylo"))
@@ -109,7 +179,7 @@ $(".Player").on("click", function() {
 			$(this).addClass("Defender");
 			playerK.playerLock = true;
 			$(".pos2").replaceWith(this);
-			$("h1").replaceWith($("<h1>Choose Your Enemy</h1>"));
+			$("h1").replaceWith($("<h1>Prepare For Battle</h1>"));
 			enemy = playerK;
 			}
 			console.log (enemy.name);
@@ -121,6 +191,7 @@ $(".Player").on("click", function() {
 		{
 			$(this).addClass("Attacker");
 			playerD.isPlayer = true;
+			console.log(this);
 			$(".pos1").replaceWith(this);
 			$("h1").replaceWith($("<h1>Choose Your Enemy</h1>"));
 			play = playerD;
@@ -152,33 +223,34 @@ $(".Player").on("click", function() {
 		}
 		console.log (play.name);	
 	}
-	
-	
-
-
 });// end of your player select
+
+
 $(".Attack").on("click", function() {
 
-	if (play.health <= 0) {
-		$("h1").replaceWith($("<h1 style=color:blue;><strong>You Lost!</strong></h1>"));
-	}
-
-
-	if(enemy.health <= 0) {
-		$("h1").replaceWith($("<h1 style=color:#26FF18;><strong>You Won! Select Another Enemy</strong></h1>"));
-	}
-	else {
 		console.log(play.attack);
-
+		
 		enemy.health  -= play.attack; 
 		play.health -= enemy.counter;
 		$(".Attacker .HP").html(play.health);
 		$(".Defender .HP").html(enemy.health);
-		play.attack = play.attackPWUP();	
-	}
-
+		play.attack = play.attackPWUP();
+		checkHealth(); //call function checkHealth
+	    
 
 });//end of attack event click
+
+
+
+});//end of document ready
+
+
+
+
+
+
+
+
 
 
 
@@ -219,5 +291,5 @@ $(".Attack").on("click", function() {
 
 	// }); //end of attack event
 
-});//end of document ready
+
 
