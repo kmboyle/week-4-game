9a5dcde04a18529e7d6a7b301f00093e61f7480f
kmboyle <kevin.michael.boyle@gmail.com>
2017-06-25 11:43:44 -0400

    restart working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,21 +3,6 @@
 
 $(document).ready(function() {
 
-//create a character object
-// var player = {
-// 	"name": ["Darth Vader", "Han Solo", "Luke Skywalker", "Kylo Ren"],
-// 	"health": [140, 115, 130, 110],
-// 	"attack": [24,18,24,30],
-// 	"counter": [18,24,24,18],
-// 	attackPWUP: function(attack) {
-// 		return attack += attack;
-// 	},
-// 	"isEnemy": false,
-// 	"isPlayer": false,
-// 	"playerLock": false
-// }
-//create each player
-
 
 var playerD = {
 				name: "Darth", 
@@ -69,110 +54,56 @@ var play;//to hold players choice
 var enemy;//to hold enemy
 var count = 3; //hold enemy count
 
-//if player wins, have them pick another enemy.
-
-// }
-
-//$(".Health").html(player.name[0].health[0]);
-function restart(play, enemy){
+function restart(){
 
 	$(".restart").html("<button>Restart</button>");
+	$(".restart").show("<button>Restart</button>");
 
 	$(".restart").on("click",function(){
-
-	var playerD = {
-				name: "Darth", 
-				health: 140, 
-				attack: 12, 
-				counter: 14, 
-				attackPWUP: function(){
-				return this.attack += 6;
-	            },
-				isEnemy: false, 
-				isPlayer: false, 
-				playerLock:false };
-var playerH = {
-				name: "Han Solo", 
-				health: 115, 
-				attack: 6, 
-				counter: 10, 
-				attackPWUP: function(){
-				return this.attack += 4;
-	            },
-				isEnemy: false, 
-				isPlayer: false, 
-				playerLock:false };
-var playerL = {
-				name: "Luke Skywalker", 
-				health: 130, 
-				attack: 8, 
-				counter: 6, 
-				attackPWUP: function(){
-				return this.attack += 8;
-	            },
-				isEnemy: false, 
-				isPlayer: false, 
-				playerLock:false };
-var playerK = {
-				name: "Kylo Ren", 
-				health: 110, 
-				attack: 10, 
-				counter: 12, 
-				attackPWUP:function(){
-				return this.attack += 4;
-	            },
-				isEnemy: false, 
-				isPlayer: false, 
-				playerLock:false };
-
-
-				// //reset game values
-		  //  	    play.isPlayer = false;
-		  //       enemy.playerLock = false;
-		  //       play = " ";
-		  //       enemy = " ";
-		  //       enemy.health = 0;
-		  //       play.health = 0;
-		  //       play.attack = 0;
 	
-	play = " ";//to hold players choice
-	enemy = " ";//to hold enemy
-	count = 3; //hold enemy count
-
+	$(".prompt").html("<h1 class ='text-left prompt'>Choose Your Player</h1>");
     $(".Attacker").detach();
 	$(".Defender").detach();
 	$(".Player").removeClass(".Attacker");
 	$(".Player").removeClass(".Defender");
 
-	$(".DarthOrig").html($("<div class = 'Player Darth'><h4>Darth Vader</h4><img class= 'img-responsive center-block' src='assets/images/Darth_Vader1.jpg' alt='Darth'/><h4 class = 'DarthHP HP text-centered'>#</h4></div>"));
-	$(".HanOrig").html($("<div class = 'Player Han'><h4>Han Solo</h4><img class= 'img-responsive center-block' src='assets/images/han solo1.jpg' alt='Han'/><h4 class = 'HanHP HP text-centered'>#</h4></div>"));
-	$(".LukeOrig").html($("<div class = 'Player Luke'><h4>Luke Skywalker</h4><img class= 'img-responsive center-block' src='assets/images/luke skywalker1.jpg' alt='Luke'/><h4 class = 'LukeHP HP text-centered'>#</h4></div>"));
-	$(".KyloOrig").html($("<div class = 'Player Kylo'><h4>Kylo Ren</h4><img class= 'img-responsive center-block' src='assets/images/Kylo Ren1.jpg' alt='Kylo'/><h4 class = 'KyloHP HP text-centered'>#</h4></div>"));
+	$(".DarthOrig").html("<div class = 'Player Darth'><h4>Darth Vader</h4><img class= 'img-responsive center-block' src='assets/images/Darth_Vader1.jpg' alt='Darth'/><h4 class = 'DarthHP HP text-centered'>#</h4></div>");
+	$(".HanOrig").html("<div class = 'Player Han'><h4>Han Solo</h4><img class= 'img-responsive center-block' src='assets/images/han solo1.jpg' alt='Han'/><h4 class = 'HanHP HP text-centered'>#</h4></div>");
+	$(".LukeOrig").html("<div class = 'Player Luke'><h4>Luke Skywalker</h4><img class= 'img-responsive center-block' src='assets/images/luke skywalker1.jpg' alt='Luke'/><h4 class = 'LukeHP HP text-centered'>#</h4></div>");
+	$(".KyloOrig").html("<div class = 'Player Kylo'><h4>Kylo Ren</h4><img class= 'img-responsive center-block' src='assets/images/Kylo Ren1.jpg' alt='Kylo'/><h4 class = 'KyloHP HP text-centered'>#</h4></div>");
 
 	$(".DarthHP").html(playerD.health);
 	$(".HanHP").html(playerH.health);
 	$(".LukeHP").html(playerL.health);
 	$(".KyloHP").html(playerK.health);
-	$(".prompt").replaceWith($("<h1 prompt>Choose Your Enemy</h1>"));
+	
 	$(".restart").hide("<button>Restart</button>");
 	start();
 });
 }
 // console.log(player.attack[0]);
 function checkHealth(){
-
+console.log("Player " + play.name);
 		if (play.health <= 0) {
-		$(".Attacker").hide();	
-		$(".prompt").replaceWith($("<h1 prompt style=color:blue;><strong>You Lost!</strong></h1>"));
-		play.isPlayer = false;
-		enemy.playerLock = false;
-		enemy.health = 0;
-		play.health = 0;
-		play.attack = 0;
-		play = " ";
-		enemy = " ";
+		//$(".Attacker").hide();	
+		$(".prompt").html("<h1 prompt style=color:blue;><strong>You Lost!</strong></h1>");
+			   //reset game values
+			    play.attack = 0;
+			    count = 3;
+		   	    play.isPlayer = false;
+		        enemy.playerLock = false;
+		        play = " ";
+		        enemy = " ";
+		        enemy.health = 0;
+		        play.health = 0;
+		        playerD.health = 140;
+				playerH.health = 115;
+				playerL.health = 130;
+				playerK.health = 110;
+				
+			 console.log("Player " + play.attack);
 			restart();
-			$(".prompt").replaceWith($("<h1 prompt>Choose Your Enemy</h1>"));
+			
 	}
 
 	if (enemy.health <= 0) {
@@ -180,23 +111,29 @@ function checkHealth(){
 		$(".prompt").html("<h1 prompt style=color:#26FF18;><strong>You Won! Select Another Enemy</strong></h1>");
 		enemy.playerLock = false;
 		$(".Defender").detach();
-		$(".Defender").hide();
+		//$(".Defender").hide();
 		if (count === 0){
-		   	$(".prompt").replaceWith($("<h1 prompt style=color:#26FF18;><strong>CONGRATULATIONS, YOU BEAT THE GAME....credits: KB</strong></h1>"));
-		   		$(".Attacker").hide();
-		   	    restart();
-		   	    $(".prompt").replaceWith($("<h1 prompt>Choose Your Enemy</h1>"));
+		   	$(".prompt").html("<h1 prompt style=color:#26FF18;><strong>CONGRATULATIONS, YOU BEAT THE GAME....credits: KB</strong></h1>");
+		   		//$(".Attacker").hide();
+		   	    //reset game values
+		   	    play.attack = 0;
+			    count = 3;
+			 	play.isPlayer = false;
+		        enemy.playerLock = false;
+		        play = " ";
+		        enemy = " ";
+		        enemy.health = 0;
+		        play.health = 0;
+		        playerD.health = 140;
+				playerH.health = 115;
+				playerL.health = 130;
+				playerK.health = 110;
+				restart();
 		   }
 		}
 	}
 
-//if player loses, have the game reset.
 
-
-
-//if player defeats all enemies, award game won and then reset.
-
-// console.log(player.attackPWUP(player.attack[0]));
 	$(".DarthHP").html(playerD.health);
 	$(".HanHP").html(playerH.health);
 	$(".LukeHP").html(playerL.health);
@@ -208,9 +145,10 @@ function start(){
 
 $(".Player").on("click", function() {
 
+	
+	//this is for the defender choice.  First time through, this will pass and go to player choice
 	if (playerD.isPlayer || playerH.isPlayer || playerK.isPlayer || playerL.isPlayer) {
-      if(!$(this).hasClass("Attacker") &&  $(".pos2").is(":empty")){
-      console.log("inside function");						
+      if(!$(this).hasClass("Attacker") &&  $(".pos2").is(":empty")) {						
 		if ($(this).hasClass("Darth"))
 		{
 			$(this).addClass("Defender");
@@ -246,7 +184,7 @@ $(".Player").on("click", function() {
 			console.log (enemy.name);
 	      }
 	}
-
+	//this is player choice
 	if (!playerD.isPlayer && !playerH.isPlayer && !playerK.isPlayer && !playerL.isPlayer) {
 
 		if ($(this).hasClass("Darth"))
@@ -289,9 +227,10 @@ $(".Player").on("click", function() {
 	
 });// end of your player select
 }
+
 function attack(){
 $(".Attack").on("click", function() {
-
+		console.log (play.name);
 		console.log(play.attack);
 		if (enemy.playerLock){
 		enemy.health  -= play.attack; 
@@ -307,9 +246,10 @@ $(".Attack").on("click", function() {
 
 });//end of attack event click
 }
+//call start function to begin game
 start();
+//call attack function next, and this will run through health check and restart functions
 attack();
-
 
 });//end of document ready
 
